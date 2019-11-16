d8cae6748f32c5c3c1f38d74cf1fdbb6f20d114f
Aman Sultan Baig <amansultanbaig@gmail.com>
2019-11-16 20:40:01 +0500

    Create Basic.ts

---
+++ Basic.ts
@@ -0,0 +1,22 @@
+// TS is types dependent language
+// Your can not give string to number data type 
+
+let myNumber: Number; 
+let myString: String;
+let myBoolean: Boolean;
+
+// you can initilize decimal, nagetive & positive values in Number Data Type
+myNumber = 23; 
+// you can concat two different string together
+myString = 'Aman Sultan Baig';
+myBoolean = true;
+
+console.log(myNumber);
+console.log(myString);
+console.log(myBoolean);
+
+
+// type command tsc Basic.ts and compile into Core JS
+
+
+
