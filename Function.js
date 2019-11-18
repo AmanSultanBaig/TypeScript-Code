4fa0d043b37332e2bdf406aa8a9a53429992e042
Aman Sultan Baig <amansultanbaig@gmail.com>
2019-11-18 15:11:50 +0500

    Create Function.js

---
+++ Function.js
@@ -0,0 +1,15 @@
+// TS is types dependent language
+// write ES6 function with data type
+function getNumber(num1, num2) {
+    return num1 + num2;
+}
+// function call 
+getNumber(2, 5);
+// write ES6 function with data type
+function getString(firstName, lastName) {
+    return firstName + lastName;
+}
+// function call 
+getString('Aman Sultan', 'Baig');
+
+// type command tsc Function.ts and compile into Core JS
