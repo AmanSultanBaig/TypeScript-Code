dab31a467a1ae85a6c83345c05613a19fcea89d1
Aman Sultan Baig <amansultanbaig@gmail.com>
2019-11-18 15:11:25 +0500

    Update Function.ts

--- Function.ts
+++ Function.ts
@@ -1,15 +1,21 @@
 // TS is types dependent language
 // write ES6 function with data type
-function getNumber(num1, num2) {
+function getNumber(num1:number, num2:number):number {
     return num1 + num2;
 }
+
 // function call 
-getNumber(2, 5);
+getNumber(2,5);
+
 // write ES6 function with data type
-function getString(firstName, lastName) {
+function getString(firstName:string, lastName:string):string {
     return firstName + lastName;
 }
+
 // function call 
-getString('Aman Sultan', 'Baig');
+getString('Aman Sultan','Baig');
 
 // type command tsc Function.ts and compile into Core JS
+
+
+
